ecd271c72d1c46bc0e4f90ec6243c8f9da8fdcf7
imbjdd <b0bb1e@protonmail.com>
2025-05-20 13:19:05 +0200

    feat: add umami stats

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -59,6 +60,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="bf8b6d22-b421-4176-975b-35d623527a81"
+          strategy="afterInteractive"
+          defer
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
